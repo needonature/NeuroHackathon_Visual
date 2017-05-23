6d46d234d773c3237f569d7b495a4c6a1d268055
AlbertYuChen <chenyu610103@gmail.com>
2017-05-22 22:55:22 -0400

    hao

--- map_am_and_pm_neuro_YC.m
+++ map_am_and_pm_neuro_YC.m
@@ -1,13 +1,13 @@
 close all; clear;clc
+
 Work_path = '/Users/chenyu/Workspace/Nuerohackason2017/';
 
 load([Work_path 'Data/2105_NC_170516_PLANE1_PINKNOISE_dataOut.mat'])
 
-str = 'Out';
 
 a = dataOut.trial(1).segmentmask;
 
-g=figure;
+figure;
 a_=[];
 b_=[];
 labels={};
@@ -28,11 +28,47 @@ a(a == -1) = 1;
 a = mat2gray(a);
 imshow(a)
 hold on
-plot(b_,a_,'r*');
+plot(b_,a_,'r*'); 
+hold on
+grid on
 text(b_ + 5,a_,labels, 'Color', [0.7 0.7 0]);
 
-% imwrite(g,[str,'.png']);
-% saveas(g,[str,'.png']);
+
+
+
+
+
+load([Work_path 'Data/2105_NC_170518_PLANE1_PINKNOISE_dataOut.mat'])
+
+a = dataOut.trial(1).segmentmask;
+
+figure
+a_=[];
+b_=[];
+labels={};
+for i=1:max(a(:))
+% for i=1:2
+	[x,y]=find(a==i);
+	xx=mean(x);
+	yy=mean(y);
+	a_=[a_,xx];
+	b_=[b_,yy];
+	labels{i}=num2str(i);
+end
+
+
+a(a == 0) = -1;
+a(a > 0) = 0;
+a(a == -1) = 1;
+a = mat2gray(a);
+imshow(a)
+hold on
+plot(b_,a_,'go'); 
+grid on
+text(b_ + 5,a_,labels, 'Color', [0.7 0.7 0]);
+
+
+
 
 
 
